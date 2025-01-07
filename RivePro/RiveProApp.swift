65b614b93f7e22108649bd607f2ab9f0680f371c
Sothesom <151612168+sotheso@users.noreply.github.com>
2025-01-07 17:13:43 +0330

    Initial Commit

---
+++ RivePro/RiveProApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiveProApp.swift
+//  RivePro
+//
+//  Created by Sothesom on 18/10/1403.
+//
+
+import SwiftUI
+
+@main
+struct RiveProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
