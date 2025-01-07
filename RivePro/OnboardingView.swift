ae5bf9653d6e6a8b2d2779dd043b72ed58b480f2
Sothesom <151612168+sotheso@users.noreply.github.com>
2025-01-07 18:03:35 +0330

    Background Animation in Rive: اضافه کردن فایل Rive انیمیشن به بکگراند صفحه

---
+++ RivePro/OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView.swift
+//  RivePro
+//
+//  Created by Sothesom on 18/10/1403.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
