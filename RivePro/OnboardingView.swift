099e972b57c6853632f4bf898d2ce9ed900cd0c7
Sothesom <151612168+sotheso@users.noreply.github.com>
2025-01-08 11:48:13 +0330

    Custom Fonts: ساختن و اضافه کردن فونت‌های کاستوم برای اپ

--- RivePro/OnboardingView.swift
+++ RivePro/OnboardingView.swift
@@ -9,15 +9,54 @@ import SwiftUI
 import RiveRuntime
 
 struct OnboardingView: View {
+    let button = RiveViewModel(fileName: "button")
+    
+    
     var body: some View {
+        ZStack {
+            backgrandBlur()
+            
+            VStack {
+                Text("خیلی خوش آمدید، لطفا حساب باز کنید")
+                    .customFont(.title )
+                    .frame(width: 260)
+                    .multilineTextAlignment(.trailing)
+                
+                Text("برای دنبال کردن روزنامه‌ها و مجلات نیاز به حساب کاربری است. شما با ساخت حساب کاربری علاوه بر دنبال کردن محتوای دلخواه خود، میتوانید به آرشیو و دانلود محتوا هم دسترسی پیدا کنید.")
+                    .customFont(.body2)
+                    .padding(.horizontal)
+                    .multilineTextAlignment(.trailing)
+                
+                button.view()
+                    .frame(width: 236, height: 64)
+                    .overlay(
+                        Label("Get Started", systemImage: "arrow.foqward")
+                            .offset(x: 4, y:4)
+                            .font(.headline)
+                    )
+                    .background(
+                        Color.black
+                            .cornerRadius(30)
+                            .blur(radius: 10)
+                            .opacity(0.3)
+                            .offset(y:10)
+                    )
+                    .onTapGesture {
+                        button.play(animationName: "active")
+                    }
+            }
+        }
+    }
+    
+    func backgrandBlur () -> some View {
         RiveViewModel(fileName: "shapes").view()
             .ignoresSafeArea()
             .blur(radius: 30)
-            .background {
+            .background(
                 Image("Spline")
                     .blur(radius: 50)
                     .offset(x: 200, y:100)
-            }
+            )
     }
 }
 
