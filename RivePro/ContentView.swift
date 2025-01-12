0d9cb3fe758f8616041f139ceefff18c2dc9df97
Sothesom <151612168+sotheso@users.noreply.github.com>
2025-01-12 14:47:15 +0330

    Icon (buger) Animation in "ASLI" View

--- RivePro/ContentView.swift
+++ RivePro/ContentView.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
+import RiveRuntime
 
 struct ContentView: View {
     @AppStorage("selectedTab") var selectedTab: Tab = .chat
+    
+    @State var isOpen = false
+    let button = RiveViewModel(fileName: "menu_button",stateMachineName: "State Machine", autoPlay: false)
+    
     
     var body: some View {
         ZStack {
@@ -24,6 +29,17 @@ struct ContentView: View {
             case .bell:
                 Text("bell")
             }
+            
+            button.view()
+                .frame(width: 44, height: 44)
+                .mask(Circle())
+                .shadow(color: Color("Shadow").opacity(0.2), radius: 5, x:0, y: 5)
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                .padding()
+                .onTapGesture {
+                    button.setInput("isOpen", value: isOpen)
+                    isOpen.toggle()
+                }
             
             TabBar()
         }
