9650cf8c9d13203e93533c831f1e778675287545
Sothesom <151612168+sotheso@users.noreply.github.com>
2025-01-13 09:11:57 +0330

    create baner VCard - part2

--- RivePro/Style/CustomFont.swift
+++ RivePro/Style/CustomFont.swift
@@ -5,9 +5,6 @@
 //  Created by Sothesom on 19/10/1403.
 //
 
-import SwiftUI
-
-
 // این کد اولیه است
 
 //struct CustomFont: ViewModifier {
@@ -15,7 +12,7 @@ import SwiftUI
 //    func body(content: Content) -> some View {
 //            switch textStyle {
 //            case .title:
-//                content.font(.custom("Hamrah-FD", size: 50, relativeTo: .title))
+//                content.font(.custom("Peyda-Black", size: 50, relativeTo: .title))
 //            case .bady1:
 //                content.font(.custom("Peyda-Regular", size: 17, relativeTo: .body))
 //            case .body2:
@@ -38,78 +35,82 @@ import SwiftUI
 //
 
 /// حالت استاندارد
+
+import SwiftUI
+
+
 struct CustomFont: ViewModifier {
-var textStyle: TextStyle
-
-var name: String {
-    switch textStyle {
-    case .largeTitle, .title, .title2, .title3:
-        return "Hamrah-FD"
-    case .body, .subheadline, .footnote, .caption:
-        return "Peyda-Regular"
-    case .headline, .subheadline2, .footnote2, .caption2:
-        return "Peyda-SemiBold"
+    var textStyle: TextStyle
+    
+    var name: String {
+        switch textStyle {
+        case .largeTitle, .title, .title2, .title3:
+            return "Peyda-Black"
+        case .body, .subheadline, .footnote, .caption:
+            return "Peyda-Regular"
+        case .headline, .subheadline2, .footnote2, .caption2:
+            return "Peyda-Semibold"
+        }
     }
-}
-
-var size: CGFloat {
-    switch textStyle {
-    case .largeTitle:
-        return 34
-    case .title:
-        return 28
-    case .title2:
-        return 24
-    case .title3:
-        return 20
-    case .body:
-        return 17
-    case .headline:
-        return 17
-    case .subheadline:
-        return 15
-    case .subheadline2:
-        return 15
-    case .footnote:
-        return 13
-    case .footnote2:
-        return 13
-    case .caption:
-        return 12
-    case .caption2:
-        return 12
+    
+    var size: CGFloat {
+        switch textStyle {
+        case .largeTitle:
+            return 34
+        case .title:
+            return 28
+        case .title2:
+            return 24
+        case .title3:
+            return 20
+        case .body:
+            return 17
+        case .headline:
+            return 17
+        case .subheadline:
+            return 15
+        case .subheadline2:
+            return 15
+        case .footnote:
+            return 13
+        case .footnote2:
+            return 13
+        case .caption:
+            return 12
+        case .caption2:
+            return 12
+        }
     }
-}
-
-var relative: Font.TextStyle {
-    switch textStyle {
-    case .largeTitle:
-        return .largeTitle
-    case .title:
-        return .title
-    case .title2:
-        return .title2
-    case .title3:
-        return .title3
-    case .body:
-        return .body
-    case .headline:
-        return .headline
-    case .subheadline:
-        return .subheadline
-    case .subheadline2:
-        return .subheadline
-    case .footnote:
-        return .footnote
-    case .footnote2:
-        return .footnote
-    case .caption:
-        return .caption
-    case .caption2:
-        return .caption
+    
+    var relative: Font.TextStyle {
+        switch textStyle {
+        case .largeTitle:
+            return .largeTitle
+        case .title:
+            return .title
+        case .title2:
+            return .title2
+        case .title3:
+            return .title3
+        case .body:
+            return .body
+        case .headline:
+            return .headline
+        case .subheadline:
+            return .subheadline
+        case .subheadline2:
+            return .subheadline
+        case .footnote:
+            return .footnote
+        case .footnote2:
+            return .footnote
+        case .caption:
+            return .caption
+        case .caption2:
+            return .caption
+        }
     }
-}
-
+    
     func body(content: Content) -> some View {
         content.font(.custom(name, size: size, relativeTo: relative))
     }
@@ -117,7 +118,7 @@ var relative: Font.TextStyle {
 
 
 extension View {
-    func CustomFont(_ textStyle: TextStyle) -> some View {
+    func customFont(_ textStyle: TextStyle) -> some View {
         modifier(CustomFont(textStyle: textStyle))
     }
 }
@@ -137,4 +138,3 @@ enum TextStyle {
     case caption
     case caption2
 }
-
--- RivePro/VCard.swift
+++ RivePro/VCard.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Sothesom on 23/10/1403.
 //
+// برای نشان دادن محتوا از دیتا های سمپل استفاده شده
 
 import SwiftUI
 
@@ -12,13 +13,52 @@ struct VCard: View {
     var course: Course
     
     var body: some View {
-        VStack{
+        VStack(alignment: .leading, spacing: 8){
+            Text(course.title)
+                .customFont(.title2)
+                .frame(maxWidth: 150, alignment: .leading)
+// اولویت داشتن عنوان بر زیر نویس
+                .layoutPriority(1)
+                
+            Text(course.subtitle)
+                .opacity(0.7)
+                .customFont(.subheadline)
+                .frame(maxWidth: .infinity, alignment: .leading)
             
+            Text(course.caption.uppercased())
+                .customFont(.footnote2)
+            
+            Spacer()
+            
+            HStack{
+// برای انداختن یک اواتا روی اوتار بعد باید یک مقدار شاخص وجود داشته باشد پس:
+                ForEach(Array([4,5,6].shuffled().enumerated()), id: \.offset) { index, number in
+                    Image("Avatar \(number)")
+                        .resizable()
+                        .frame(width: 44, height: 44)
+                        .mask(Circle())
+                        .offset(x: CGFloat(index * -20))
+                }
+            }
         }
+        .foregroundColor(.white)
+        .padding(30)
+        .frame(width: 260, height: 310)
+        .background(.linearGradient(colors: [course.color, course.color.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
+        .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
+        .shadow(color: course.color.opacity(0.3), radius: 8, x: 0, y: 12)
+        .shadow(color: course.color.opacity(0.3), radius: 2, x: 0, y: 1)
+        
+        .overlay(
+            course.image
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
+                .padding(20)
+        )
+        
         
     }
 }
 
 #Preview {
-    VCard(course: courses[0])
+    VCard(course: courses[2])
 }
